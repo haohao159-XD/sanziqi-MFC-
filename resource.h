e2d8117df3769b7f828b01dd0474275b13b252c8
haohao159 <mchaohao159@outlook.com>
2022-09-30 23:55:06 +0800

    1112

--- resource.h
+++ resource.h
@@ -13,6 +13,7 @@
 #define IDI_SMALL                       108
 #define IDC_WINDOWSPROJECT2             109
 #define IDR_MAINFRAME                   128
+#define IDI_ICON1                       130
 #define IDB_OK                          1000
 #define IDB_CANCEL                      1001
 #define IDB_initializeGAME              1002
@@ -34,7 +35,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           110
